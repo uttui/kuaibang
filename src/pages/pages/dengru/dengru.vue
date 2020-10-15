6471e7ed99c660686b40b22d74bade1226391c56
uttui <2136631388@qq.com>
2020-10-15 10:50:53 +0800

    解决登入、首页页面垂直居中问题

---
+++ src/pages/pages/dengru/dengru.vue
@@ -0,0 +1,159 @@
+<template>
+  <view class="page">
+    <view class="title">快帮网</view>
+    <view class="s-title">干啥都行，干啥都快</view>
+    <view class="shouji">
+      <view class="wrap">
+        <image class="icon" src="/static/shouji.png" />
+        <input type="text" class="input" placeholder="手机号码" />
+      </view>
+    </view>
+    <view class="yanzheng">
+      <view class="wrap">
+        <view class="left">
+          <image class="icon" src="/static/duanxin.png" />
+          <input type="text" class="input" placeholder="短信验证码" />
+        </view>
+        <view class="btn">获取动态码</view>
+      </view>
+    </view>
+    <view class="btn-dengru">
+      <view class="btn">登入</view>
+    </view>
+    <view class="btn-zhuce">
+      <view class="btn">立即注册</view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.page {
+  .title {
+    text-align: center;
+    margin-top: 100px;
+
+    height: 33px;
+    font-size: 24px;
+
+    font-weight: 500;
+    color: #ff552e;
+    line-height: 33px;
+  }
+  .s-title {
+    text-align: center;
+    margin-top: 10px;
+    height: 20px;
+    font-size: 14px;
+    font-weight: 400;
+    color: #ff552e;
+    line-height: 20px;
+  }
+  .shouji {
+    margin-top: 40px;
+
+    .wrap {
+      width: 80%;
+      height: 44px;
+      margin-left: auto;
+      margin-right: auto;
+      display: flex;
+      border-radius: 22px;
+      border: 3px solid #eeeeee;
+
+      .icon {
+        width: 11px;
+        height: 18px;
+        padding-left: 15px;
+        align-self: center;
+      }
+      .input {
+        margin-left: 10px;
+        border: none;
+        color: #cccccc;
+        align-self: center;
+      }
+    }
+  }
+  .yanzheng {
+    margin-top: 20px;
+
+    .wrap {
+      width: 80%;
+      margin-left: auto;
+      margin-right: auto;
+      display: flex;
+      justify-content: space-between;
+
+      .left {
+        width: 50%;
+        height: 44px;
+        border-radius: 22px;
+        border: 3px solid #eeeeee;
+        display: flex;
+
+        .icon {
+          width: 17px;
+          height: 14px;
+          padding-left: 15px;
+          align-self: center;
+        }
+
+        .input {
+          margin-left: 5px;
+          font-size: 14px;
+          border: none;
+          width: 80px;
+          color: #cccccc;
+          align-self: center;
+        }
+      }
+
+      .btn {
+        text-align: center;
+        line-height: 44px;
+        width: 40%;
+        height: 44px;
+        border-radius: 22px;
+        border: 3px solid #eeeeee;
+        font-size: 14px;
+        font-weight: 500;
+        color: #0e8ed8;
+      }
+    }
+  }
+  .btn-dengru {
+    margin-top: 30px;
+
+    .btn {
+      width: 80%;
+      margin-left: auto;
+      margin-right: auto;
+      text-align: center;
+      height: 44px;
+      background: #ff552e;
+      color: white;
+      border-radius: 22px;
+      font-size: 20px;
+      line-height: 44px;
+    }
+  }
+  .btn-zhuce {
+    margin-top: 40px;
+
+    .btn {
+      width: 56px;
+      margin-left: auto;
+      margin-right: auto;
+      height: 20px;
+      font-size: 14px;
+      font-weight: 400;
+      color: #0e8ed8;
+      line-height: 20px;
+    }
+  }
+}
+</style>
---
+++ src/pages/pages/fabu/fabu.vue
@@ -0,0 +1,139 @@
+<template>
+  <view class="page">
+    <view class="title">请选择发布类别</view>
+    <view class="box">
+      <view class="s-box">
+        <image class="icon" src="/static/icon/zhaodian.png" />
+        <view class="biaoqian">房产</view>
+      </view>
+      <view class="text">
+        <view class="text-title">商铺转让，招租求租</view>
+        <view class="s-text">线上线下，渠道全覆盖</view>
+      </view>
+      <image class="jiantou" src="/static/icon/jiantou-2.png" />
+    </view>
+    <view class="box">
+      <view class="s-box">
+        <image class="icon" src="/static/icon/ping.png" />
+        <view class="biaoqian">招聘</view>
+      </view>
+      <view class="text">
+        <view class="text-title">帮我招聘</view>
+        <view class="s-text">人才全聚集，招人好轻松</view>
+      </view>
+      <image class="jiantou" src="/static/icon/jiantou-2.png" />
+    </view>
+    <view class="box">
+      <view class="s-box">
+        <image class="icon" src="/static/icon/qiuzhi.png" />
+        <view class="biaoqian">求职</view>
+      </view>
+      <view class="text">
+        <view class="text-title">想找工作</view>
+        <view class="s-text">找我就对了</view>
+      </view>
+      <image class="jiantou" src="/static/icon/jiantou-2.png" />
+    </view>
+    <view class="box">
+      <view class="s-box">
+        <image class="icon" src="/static/icon/bang.png" />
+        <view class="biaoqian">万能帮</view>
+      </view>
+      <view class="text">
+        <view class="text-title">便民服务</view>
+        <view class="s-text">体贴您的服务</view>
+      </view>
+      <image class="jiantou" src="/static/icon/jiantou-2.png" />
+    </view>
+    <view class="box">
+      <view class="s-box">
+        <image class="icon" src="/static/icon/ershou.png" />
+        <view class="biaoqian">二手交易</view>
+      </view>
+      <view class="text">
+        <view class="text-title">立马变现</view>
+        <view class="s-text">线上线下，样样我在行</view>
+      </view>
+      <image class="jiantou" src="/static/icon/jiantou-2.png" />
+    </view>
+    <view class="box">
+      <view class="s-box">
+        <image class="icon" src="/static/icon/kuaigou.png" />
+        <view class="biaoqian">快购</view>
+      </view>
+      <view class="text">
+        <view class="text-title">买东西，急急急</view>
+        <view class="s-text">便宜快捷，为您服务</view>
+      </view>
+      <image class="jiantou" src="/static/icon/jiantou-2.png" />
+    </view>
+    <view class="box">
+      <view class="s-box">
+        <image class="icon" src="/static/icon/aixin.png" />
+        <view class="biaoqian">爱心公益</view>
+      </view>
+      <view class="text">
+        <view class="text-title">上善若水</view>
+        <view class="s-text">勿以善小而不为</view>
+      </view>
+      <image class="jiantou" src="/static/icon/jiantou-2.png" />
+    </view>
+  </view>
+</template>
+
+
+<style lang="scss" scoped>
+.page {
+  margin-top: 10px;
+  .title {
+    margin-left: 30px;
+    font-size: 14px;
+    font-weight: 400;
+    color: #333333;
+    line-height: 20px;
+  }
+  .box {
+    margin-left: 7%;
+    margin-top: 10px;
+    display: flex;
+    height: 80px;
+    width: 85%;
+    border-bottom: #eeeeee 1px solid;
+    .s-box {
+      margin-top: 14px;
+      margin-left: 6px;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
+      .biaoqian {
+        margin-top: 5px;
+        text-align: center;
+        font-size: 12px;
+        color: #666666;
+      }
+    }
+    .text {
+      margin-top: 16px;
+      margin-left: 37px;
+      .text-title {
+        font-size: 14px;
+        font-weight: bold;
+        color: #333333;
+      }
+      .s-text {
+          margin-top: 3px;
+        font-size: 12px;
+        color: #999999;
+      }
+    }
+    .jiantou {
+      margin-top: 29px;
+      margin-left: 90px;
+      margin-right: 6px;
+      width: 8px;
+      height: 15px;
+    }
+  }
+}
+</style>
